c5e35f2e3b9ce8da1a4b112fc4706661f487faf5
Luíza <luizaesteves987@gmail.com>
2021-10-05 09:58:13 -0300

    busca por roupa cadastrada

--- src/main/java/view/TelaDetalheVenda.java
+++ src/main/java/view/TelaDetalheVenda.java
@@ -9,8 +9,19 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import controle.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
-public class TelaDetalheVenda implements ActionListener{
+public final class TelaDetalheVenda implements ActionListener, MouseListener, ListSelectionListener{
 	private JFrame janela;
 	
 	private final JLabel labelCod = new JLabel("Código da compra: ");
@@ -25,21 +36,34 @@ public class TelaDetalheVenda implements ActionListener{
 	private JTextField valorCliente;
 	private final JLabel labelFuncionario = new JLabel("Funcionário: ");
 	private JTextField valorFuncionario;
-	
+        
+        private JFrame j;
+        private JLabel t;
+        
+        private JList<String> listaRoupasCadastrados;
+
 	private final JButton botaoExcluir = new JButton("Excluir");
 	private final JButton botaoSalvar = new JButton("Salvar");
-	
+
 	private String[] novoDado = new String[9];
 	private static ControleDados dados;
 	private int posicao;
 	private int opcao;
 	private String s;
-	
+        
+   
+               
+
+        
+        
 	public void inserirEditar(int op, ControleDados d, TelaVenda v, int pos) {
+                
 		opcao = op;
 		posicao = pos;
 		dados = d;
-		
+	     
+               
+                
 		if (op == 1) s = "Cadastro de venda";
 		if (op == 2) s = "Detalhe de venda";
 
@@ -53,14 +77,17 @@ public class TelaDetalheVenda implements ActionListener{
 			valorCliente = new JTextField(String.valueOf(dados.getVenda()[pos].getCliente().getNome()), 200);
 			valorFuncionario = new JTextField(String.valueOf(dados.getVenda()[pos].getFuncionario().getNome()), 200);		
 		} else {
-
+                        
+                        
 			valorCod = new JTextField(200);
 			valorRoupa = new JTextField(200);
 			valorValor = new JTextField(200);
 			valorForma = new JTextField(200);
 			valorCliente = new JTextField(200);
+                     
 			valorFuncionario = new JTextField(200);
                         
+                       
 			botaoSalvar.setBounds(245, 300, 115, 30);
                         botaoSalvar.addActionListener(this);
 		}
@@ -69,6 +96,7 @@ public class TelaDetalheVenda implements ActionListener{
 		valorCod.setBounds(180, 20, 180, 25);
 		labelNomeRoupa.setBounds(30, 60, 150, 25);
 		valorRoupa.setBounds(180, 60, 180, 25);
+                valorRoupa.addMouseListener(this);
 		labelValorTotal.setBounds(30, 100, 180, 25);
 		valorValor.setBounds(180, 100, 180, 25);		
 		labelForma.setBounds(30, 140, 150, 25);
@@ -77,6 +105,7 @@ public class TelaDetalheVenda implements ActionListener{
 		valorCliente.setBounds(180, 180, 180, 25);
 		labelFuncionario.setBounds(30, 220, 150, 25);
 		valorFuncionario.setBounds(180, 220, 180, 25);
+                
 		
 		if (op == 2) {
 			botaoSalvar.setBounds(120, 300, 115, 30);
@@ -97,6 +126,8 @@ public class TelaDetalheVenda implements ActionListener{
 		this.janela.add(labelFuncionario);
 		this.janela.add(valorFuncionario);
 		this.janela.add(botaoSalvar);
+                
+  
 
 		this.janela.setLayout(null);
 
@@ -147,8 +178,11 @@ public class TelaDetalheVenda implements ActionListener{
 				else mensagemErroExclusao(); 
 						
 		}
-	}
-	
+                                
+
+        }
+   
+
 	public void mensagemSucessoExclusao() {
 		JOptionPane.showMessageDialog(null, "Os dados foram excluidos com sucesso!", null, 
 				JOptionPane.INFORMATION_MESSAGE);
@@ -176,5 +210,109 @@ public class TelaDetalheVenda implements ActionListener{
 	
 	
         }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Object src = e.getSource();
+        if(src==valorRoupa){
+                    
+                    j = new JFrame("Opções roupa");
+                    t = new JLabel("Opções roupa");
+                    ArrayList<String> todasRoupas= new ArrayList();
+                    
+                      
+                    ControleCalca cal = new ControleCalca(dados);
+                    ControleCamisa cam = new ControleCamisa(dados);
+                    ControleCamiseta cami  = new ControleCamiseta(dados);
+                    ControleCasaco cas = new ControleCasaco(dados);
+                    ControleCinto cin = new ControleCinto(dados);
+                    ControleCropped cro = new ControleCropped(dados);
+                    ControleMacacao mac = new ControleMacacao(dados);
+                    ControleSaia sai= new ControleSaia(dados);
+                    ControleShorte sho = new ControleShorte(dados);
+                    for(int x=0;x<cal.getNomesCalcas().length;x++){
+                        todasRoupas.add(cal.getNomesCalcas()[x]);
+                    }
+                    for(int x=0;x<cam.getNomesCamisa().length;x++){
+                        todasRoupas.add(cam.getNomesCamisa()[x]);
+                    }
+                    for(int x=0;x<cami.getNomesCamiseta().length;x++){
+                        todasRoupas.add(cami.getNomesCamiseta()[x]);
+                    }
+                    for(int x=0;x<cas.getNomesCasaco().length;x++){
+                        todasRoupas.add(cas.getNomesCasaco()[x]);
+                    }
+                    for(int x=0;x<cin.getNomesCinto().length;x++){
+                        todasRoupas.add(cin.getNomesCinto()[x]);
+                    }
+                    for(int x=0;x<cro.getNomesCropped().length;x++){
+                        todasRoupas.add(cro.getNomesCropped()[x]);
+                    }
+                    for(int x=0;x<mac.getNomesMacacao().length;x++){
+                        todasRoupas.add(mac.getNomesMacacao()[x]);
+                    }
+                    for(int x=0;x<sai.getNomesSaia().length;x++){
+                        todasRoupas.add(sai.getNomesSaia()[x]);
+                    }
+                    for(int x=0;x<sho.getNomesShorte().length;x++){
+                        todasRoupas.add(sho.getNomesShorte()[x]);
+                    }
+                    String z[]=todasRoupas.toArray(new String[0]);
+                    
+                    listaRoupasCadastrados = new JList<String>(z);
+                    listaRoupasCadastrados.setBounds(20, 50, 400, 400);
+                    
+                    listaRoupasCadastrados.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+                    listaRoupasCadastrados.setVisibleRowCount(100);
+                    JScrollPane scroll = new JScrollPane(listaRoupasCadastrados);
+                    j.add(t);
+                    j.add(scroll);
+                    j.setSize(600, 600);
+                    j.setVisible(true);
+                    listaRoupasCadastrados.addListSelectionListener(this);
+                }
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        Object src = e.getSource();
+       
+
+		if(e.getValueIsAdjusting() && src == listaRoupasCadastrados) {
+                    
+                    valorRoupa.setText(listaRoupasCadastrados.getName());                   
+                    this.janela.add(valorRoupa);                    
+                    this.j.dispose();
+                    
+                    
+                   
+                    
+			
+		}
+        
+    }
+
+    
 }
         
